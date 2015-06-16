0c77868a0dca977902508ea98fd36468dda0452d
T-107 <T-107@PC254>
2015-06-15 19:10:29 -0500

    15/06/2015

---
+++ src/chekedexeption/ProbarCheque.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chekedexeption;
+
+/**
+ *
+ * @author T-107
+ */
+
+import java.io.*;
+
+public class ProbarCheque {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
