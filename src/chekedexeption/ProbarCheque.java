3c5f83fd8fc3d7cba3b4edd0093d2bef7595f58d
T-107 <T-107@PC254>
2015-06-15 19:43:29 -0500

    15/06/2015

--- src/chekedexeption/ProbarCheque.java
+++ src/chekedexeption/ProbarCheque.java
@@ -11,15 +11,23 @@ package chekedexeption;
  */
 
 import java.io.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ProbarCheque {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args)
+            throws IOException {
         // TODO code application logic here
-        
+        File f=new File("Archivaldo");
+       
+            FileOutputStream fos=new FilterOutputStream(f);
+            ObjectOutputStream oos=new ObjectOutputStream(fos);
+            
+       
     }
     
 }
---
+++ src/miprimeraexepcion/MenordeEdadExeption.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package miprimeraexepcion;
+
+
+public class MenordeEdadExeption extends Exception {
+    public MenordeEdadExeption(){
+        super ("Eres un chamaco pervertido, le dire a tu mama no te puedo registrar");
+        
+    }
+    
+}
